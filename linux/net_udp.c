9dee597e67cddfb2ed4b0ff72252340e34cbc884
Jonathan Gray <jsg@jsg.id.au>
2012-08-12 21:56:30 +1000

    make this compile on !linux, from FreeBSD ports

--- linux/net_udp.c
+++ linux/net_udp.c
@@ -13,8 +13,12 @@
 #include <sys/uio.h>
 #include <errno.h>
 
+#ifdef __linux__
 #include <linux/types.h>
 #include <linux/errqueue.h>
+#else
+#include <sys/types.h>
+#endif
 
 #ifdef NeXT
 #include <libc.h>
@@ -187,6 +191,7 @@ int	NET_GetPacket (netsrc_t sock, netadr_t *net_from, sizebuf_t *net_message)
 
 	if (ret == -1)
 	{
+#ifdef __linux__
 		//linux makes this needlessly complex, couldn't just return the source of the error in from, oh no...
 		struct probehdr	rcvbuf;
 		struct iovec	iov;
@@ -308,9 +313,19 @@ int	NET_GetPacket (netsrc_t sock, netadr_t *net_from, sizebuf_t *net_message)
 					continue;
 			}
 		}
+#else
+		err = errno;
 
-		//errno = err;
-		//Com_Printf ("NET_GetPacket: %s\n", LOG_NET, NET_ErrorString());
+		if (err == EWOULDBLOCK)
+			return 0;
+		if (err == ECONNREFUSED)
+		{
+			SockadrToNetadr (&from, net_from);
+			Com_Printf ("NET_GetPacket: %s from %s\n", LOG_NET, NET_ErrorString(), NET_AdrToString (net_from));
+			return -1;
+		}
+		Com_Printf ("NET_GetPacket: %s\n", LOG_NET, NET_ErrorString());
+#endif
 		return 0;
 	}
 
@@ -427,6 +442,7 @@ int NET_IPSocket (char *net_interface, int port)
 		return 0;
 	}
 
+#ifdef __linux__
 	// r1: accept icmp unreachables for quick disconnects
 	if (!net_no_recverr->intvalue)
 	{
@@ -435,6 +451,7 @@ int NET_IPSocket (char *net_interface, int port)
 			Com_Printf ("UDP_OpenSocket: Couldn't set IP_RECVERR: %s\n", LOG_NET, NET_ErrorString());
 		}
 	}
+#endif
 
 	if (!net_interface || !net_interface[0] || !Q_stricmp(net_interface, "localhost"))
 		address.sin_addr.s_addr = INADDR_ANY;
